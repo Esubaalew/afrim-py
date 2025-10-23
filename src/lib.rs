8d55c8ef9598f127377303cdf4f13f9033388f09
Brady Fomegne <brady.fomegne@outlook.com>
2025-10-23 22:48:22 +0100

    patch: implement the `afrim-config` crate
    This commit implements the python interface for the `afrim-config`.
    
    As additionnal changes:
    
    - removed the toml_to_json converter
    - updated the tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,24 @@
 #![deny(missing_docs)]
 //! Python bindings for the afrim input method engine.
 
+mod config;
 mod preprocessor;
 mod translator;
 
+pub use config::Config;
 pub use preprocessor::Preprocessor;
 pub use translator::Translator;
 
 use pyo3::prelude::*;
-use toml;
 
-/// Convert TOML to JSON (returns JSON string).
-///
-/// Example:
-/// ```python
-/// convert_toml_to_json("[info]\nname = 'sample'\n[data]\nhello = 'hi'")
-/// ```
 #[pyfunction]
-fn convert_toml_to_json(content: &str) -> PyResult<String> {
-    let data: toml::Value = toml::from_str(content)
-        .map_err(|err| pyo3::exceptions::PyValueError::new_err(format!("Invalid TOML data.\nCaused by:\n\t{err}")))?;
-    serde_json::to_string(&data)
-        .map_err(|err| pyo3::exceptions::PyValueError::new_err(format!("Serialization error: {err}")))
+fn is_rhai_feature_enabled() -> bool {
+    cfg!(feature = "rhai")
+}
+
+#[pyfunction]
+fn is_strsim_feature_enabled() -> bool {
+    cfg!(feature = "strsim")
 }
 
 /// Python module definition
@@ -29,6 +26,9 @@ fn convert_toml_to_json(content: &str) -> PyResult<String> {
 fn afrim_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Preprocessor>()?;
     m.add_class::<Translator>()?;
-    m.add_function(wrap_pyfunction!(convert_toml_to_json, m)?)?;
+    m.add_class::<Config>()?;
+    m.add_function(wrap_pyfunction!(is_rhai_feature_enabled, m)?)?;
+    m.add_function(wrap_pyfunction!(is_strsim_feature_enabled, m)?)?;
+
     Ok(())
 }
